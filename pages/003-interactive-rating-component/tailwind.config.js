da46a3df611ecb35de217ec2a55fbdf09949cf15
Dyann@Alicelise <wilhelmjanmapogi@gmail.com>
2023-01-12 06:08:52 +0800

    Add style guide on tailwind config

--- pages/003-interactive-rating-component/tailwind.config.js
+++ pages/003-interactive-rating-component/tailwind.config.js
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        orange: "hsl(25, 97%, 53%)",
+        lightGray: "hsl(217, 12%, 63%)",
+        mediumGray: "hsl(216, 12%, 54%)",
+        darkBlue: "hsl(213, 19%, 18%)",
+        veryDarkBlue: "hsl(216, 12%, 8%)",
+      },
+      fontFamily: {
+        sans: ["Overpass", ...defaultTheme.fontFamily.sans],
+      },
+      fontSize: {
+        base: "15px",
+      },
+    },
   },
   plugins: [],
 };
