cb9df7b5a500e01dd43f648a04630195c98d37a1
Dyann@Alicelise <wilhelmjanmapogi@gmail.com>
2023-06-03 21:09:51 +0800

    feat: Add design to tailwind-config

--- challenges/stats-preview-card-component/tailwind.config.js
+++ challenges/stats-preview-card-component/tailwind.config.js
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+console.log(defaultTheme.fontSize);
+
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    fontSize: {
+      base: ["15px", ...defaultTheme.fontSize],
+    },
+    extend: {
+      colors: {
+        veryDarkBlue: "hsl(233, 47%, 7%)",
+        darkDesaturatedBlue: "hsl(244, 38%, 16%)",
+        softViolet: "hsl(277, 64%, 61%)",
+        slightlyTransparentWhite: "hsla(0, 0%, 100%, 0.75)",
+        slightlyTransparentWhite2: "hsla(0, 0%, 100%, 0.6)",
+      },
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+        lexendDeca: ["Lexend Deca", ...defaultTheme.fontFamily.sans],
+        inter: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
